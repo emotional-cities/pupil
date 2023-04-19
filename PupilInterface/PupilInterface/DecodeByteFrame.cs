ce43b8a98aafde609b636df9d3f19a4a66aa7c3c
RoboDoig <erskine94@gmail.com>
2023-04-19 16:29:03 +0100

    VideoDataHeader, initial byte decoder

---
+++ PupilInterface/PupilInterface/DecodeByteFrame.cs
@@ -0,0 +1,23 @@
+﻿using Bonsai;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PupilInterface
+{
+    public class DecodeByteFrame : Transform<byte[], int>
+    {
+        public override IObservable<int> Process(IObservable<byte[]> source)
+        {
+            FFmpegBinariesHelper.RegisterFFmpegBinaries(); // TODO - this sort of stuff should probably go in some general resource node
+
+            return source.Select(val =>
+            {
+                return 1;
+            });
+        }
+    }
+}
