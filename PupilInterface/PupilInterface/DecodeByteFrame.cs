f1e324a7d1e68d65ad3b79b0fea628b293274d9e
RoboDoig <erskine94@gmail.com>
2023-04-25 14:23:54 +0100

    Working frame decoder prototype

--- PupilInterface/PupilInterface/DecodeByteFrame.cs
+++ PupilInterface/PupilInterface/DecodeByteFrame.cs
@@ -8,18 +8,35 @@ using System.Text;
 using System.Threading.Tasks;
 using FFmpeg.AutoGen.Abstractions;
 using FFmpeg.AutoGen.Bindings.DynamicallyLoaded;
+using OpenCV.Net;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
 
 namespace PupilInterface
 {
-    public class DecodeByteFrame : Transform<byte[], AVFrame>
+    public class DecodeByteFrame : Transform<byte[], IplImage>
     {
-        public override IObservable<AVFrame> Process(IObservable<byte[]> source)
+        public unsafe override IObservable<IplImage> Process(IObservable<byte[]> source)
         {
             var decoder = new VideoDecoder();
 
             return source.Select(val =>
             {
-                return decoder.DecodeFrame(val);
+                IplImage decodedFrame = decoder.DecodeFrame(val);
+
+                //IplImage image;
+                //if (decodedFrame.width > 0 && decodedFrame.height > 0)
+                //{
+                //    image = new IplImage(new OpenCV.Net.Size(decodedFrame.width, decodedFrame.height), IplDepth.U8, 1, (IntPtr)decodedFrame.data[0]);
+                //}
+                //else
+                //{
+                //    image = new IplImage(new OpenCV.Net.Size(1088, 1080), IplDepth.U8, 1);
+                //}
+
+                //return image;
+
+                return decodedFrame;
             });
         }
     }
@@ -41,7 +58,7 @@ namespace PupilInterface
             int openResult = ffmpeg.avcodec_open2(pCodecContext, pAvCodec, null);
         }
 
-        public unsafe AVFrame DecodeFrame(byte[] frame)
+        public unsafe IplImage DecodeFrame(byte[] frame)
         {
             fixed (byte* _frame = frame)
             {
@@ -54,12 +71,12 @@ namespace PupilInterface
                 AVFrame* decodedFrame = ffmpeg.av_frame_alloc();
                 int decodeFrameResult = ffmpeg.avcodec_receive_frame(pCodecContext, decodedFrame);
 
-                if (decodeFrameResult == 0)
+                if (decodeFrameResult == 0) 
                 {
                     AVFrame sourceFrame = *decodedFrame;
                     AVPixelFormat pixelFormat = pCodecContext->pix_fmt;
 
-                    Size frameSize = new Size(sourceFrame.width, sourceFrame.height);
+                    System.Drawing.Size frameSize = new System.Drawing.Size(sourceFrame.width, sourceFrame.height);
 
                     VideoFrameConverter vfc = new VideoFrameConverter(
                         frameSize,
@@ -70,16 +87,29 @@ namespace PupilInterface
 
                     AVFrame convertedFrame = vfc.Convert(sourceFrame);
 
+                    //using (var bitmap = new Bitmap(convertedFrame.width, convertedFrame.height, convertedFrame.linesize[0], PixelFormat.Format24bppRgb, (IntPtr)convertedFrame.data[0]))
+                    //{
+                    //    bitmap.Save($"frame-{counter}.jpg", ImageFormat.Jpeg);
+                    //}
+
+                    //int numBytes = ffmpeg.av_image_get_buffer_size(AVPixelFormat.AV_PIX_FMT_BGR24, convertedFrame.width, convertedFrame.height, 32);
+
+                    //byte[] copyArray = new byte[numBytes];
+                    //Marshal.Copy((IntPtr)convertedFrame.data[0], copyArray, 0, numBytes);
+
+                    IplImage image = new IplImage(new OpenCV.Net.Size(convertedFrame.width, convertedFrame.height), IplDepth.U8, 3, (IntPtr)convertedFrame.data[0]);
+                    IplImage imageCopy = image.Clone();
+
                     vfc.Dispose();
 
                     // Free packets/frames
                     ffmpeg.av_frame_unref(decodedFrame);
                     ffmpeg.av_freep(decodedFrame);
 
-                    return convertedFrame;
+                    return imageCopy;
                 }
 
-                return new AVFrame();
+                return null;
             }
         }
     }
